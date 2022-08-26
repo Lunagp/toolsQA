76fa3ff3f43610511d5222d224eaf867e9ce15b5
CarvajalAnni <carvajalanni@gmail.com>
2022-08-26 08:31:48 -0500

    buil gradle v3

--- src/main/java/co/com/prueba/demoqa/userinterfaces/ButtonHome.java
+++ src/main/java/co/com/prueba/demoqa/userinterfaces/ButtonHome.java
@@ -1,4 +1,9 @@
 package co.com.prueba.demoqa.userinterfaces;
 
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
 public class ButtonHome {
+
+    public static final Target BTN_ELEMENT = Target.the("botón opción elementos").located(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[3]/h5"));
 }
--- src/test/java/co/com/prueba/demoqa/runners/PDemoQARunner.java
+++ src/test/java/co/com/prueba/demoqa/runners/PDemoQARunner.java
@@ -1,4 +1,13 @@
 package co.com.prueba.demoqa.runners;
 
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/pdemoqa.feature",
+        glue = "co/com/prueba/demoqa/stepdefinitions"
+)
 public class PDemoQARunner {
 }
